4da40fd8a62f59dfb8e6386b656c3da8be0d5be0
Ludvig-Lundberg <ickeskola@gmail.com>
2023-02-03 09:43:12 +0100

    lagt till bortakvall grupparbetet med index.html osv

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,23 +1,22 @@
 import express from "express";
 import { body } from "express-validator";
-//import router from ".";
 import { index, show, store } from '../controllers/product_controller'
-
+//import router from "."; // ändrade till const router nedan för att det inte fungerade när jag gjorde såhär
 const router = express.Router()
 
 // alla produkter
 router.get('/', index)
 // specifik produkt
 router.get("/:productId", show)
-// post produkt
+// post:a produkt
 router.post('/', [
-    body("name").isString().withMessage("must be string"),
-    body("description").isString().withMessage("must be string"),
-    body("price").isInt().withMessage("must be a number"),
-    body("images").isObject().withMessage("must be JSON object with images"),
-    body("stock_status").isString().withMessage("must be string"),
-    body("stock_quantity").isInt().withMessage("must be a number")
+    body("name")            .isString().withMessage("must be string"),
+    body("description")     .isString().withMessage("must be string"),
+    body("price")           .isInt()   .withMessage("must be a number"),
+    body("images")          .isObject().withMessage("must be JSON object with images"),
+    body("stock_status")    .isString().withMessage("must be string"),
+    body("stock_quantity")  .isInt()   .withMessage("must be a number")
 ], store)
-//router.post('/products', store)
+
 
 export default router
